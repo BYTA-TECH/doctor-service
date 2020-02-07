5a5921fcf26ebbb28c02b825529e4989f7196d90
sarathadoor <sarath.kumar.s@lxisoft.com>
2020-02-07 11:31:13 +0530

    Swagger Alfresco

---
+++ src/main/java/com/bytatech/ayoos/doctor/client/dms/api/NetworksApiClient.java
@@ -0,0 +1,8 @@
+package com.bytatech.ayoos.doctor.client.dms.api;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import com.bytatech.ayoos.doctor.client.dms.ClientConfiguration;
+
+@FeignClient(name="${dms.name:dms}", url="${dms.url:http://localhost/alfresco/api/-default-/public/alfresco/versions/1}", configuration = ClientConfiguration.class)
+public interface NetworksApiClient extends NetworksApi {
+}
